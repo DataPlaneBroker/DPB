6b7d2d8fcdb8cfbe2b429d9a66c17cc18cee867b
simpsons <simpsons@eb0d62cf-e8a8-483d-89ee-368caa62dd19>
2019-06-26 14:03:21 +0000

    Fully extricated topology alignment process.

--- src/java-tree/tests/AlgoPerfTest.java
+++ src/java-tree/tests/AlgoPerfTest.java
@@ -40,7 +40,6 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -84,14 +83,76 @@ public class AlgoPerfTest {
 
         public double fx = 0.0, fy = 0.0;
 
-        void resetForce() {
-            fx = 0.0;
-            fy = 0.0;
-        }
-
         double mass = 1.0;
 
         private static int nextId;
+
+        static final VertexAttribution<Vertex> ATTRIBUTION =
+            new VertexAttribution<Vertex>() {
+                @Override
+                public double y(Vertex vert) {
+                    return vert.y;
+                }
+
+                @Override
+                public double x(Vertex vert) {
+                    return vert.x;
+                }
+
+                @Override
+                public double vy(Vertex vert) {
+                    return vert.vy;
+                }
+
+                @Override
+                public double vx(Vertex vert) {
+                    return vert.vx;
+                }
+
+                @Override
+                public void step(Vertex vert, double delta) {
+                    vert.vx += vert.fx * delta;
+                    vert.vy += vert.fy * delta;
+                    vert.x += vert.vx * delta;
+                    vert.y += vert.vy * delta;
+                }
+
+                @Override
+                public void resetForce(Vertex vert) {
+                    vert.fx = 0.0;
+                    vert.fy = 0.0;
+                }
+
+                @Override
+                public void move(Vertex vert, double dx, double dy) {
+                    vert.x += dx;
+                    vert.y += dy;
+                }
+
+                @Override
+                public double mass(Vertex vert) {
+                    return vert.mass;
+                }
+
+                @Override
+                public double force(Vertex vert) {
+                    final double fx = vert.fx;
+                    final double fy = vert.fy;
+                    return Math.sqrt(fx * fx + fy * fy);
+                }
+
+                @Override
+                public void diminishForce(Vertex vert) {
+                    vert.fx /= vert.mass;
+                    vert.fy /= vert.mass;
+                }
+
+                @Override
+                public void addForce(Vertex vert, double dx, double dy) {
+                    vert.fx += dx;
+                    vert.fy += dy;
+                }
+            };
     }
 
     /**
@@ -138,314 +199,10 @@ public class AlgoPerfTest {
             topModel.setData(0.0, edges);
             Thread.sleep(3 * 1000);
 
-            VertexAttribution<Vertex> attrs =
-                new VertexAttribution<Vertex>() {
-                    @Override
-                    public double y(Vertex vert) {
-                        return vert.y;
-                    }
-
-                    @Override
-                    public double x(Vertex vert) {
-                        return vert.x;
-                    }
-
-                    @Override
-                    public double vy(Vertex vert) {
-                        return vert.vy;
-                    }
-
-                    @Override
-                    public double vx(Vertex vert) {
-                        return vert.vx;
-                    }
-
-                    @Override
-                    public void step(Vertex vert, double delta) {
-                        vert.vx += vert.fx * delta;
-                        vert.vy += vert.fy * delta;
-                        vert.x += vert.vx * delta;
-                        vert.y += vert.vy * delta;
-                    }
-
-                    @Override
-                    public void resetForce(Vertex vert) {
-                        vert.fx = 0.0;
-                        vert.fy = 0.0;
-                    }
-
-                    @Override
-                    public void move(Vertex vert, double dx, double dy) {
-                        vert.x += dx;
-                        vert.y += dy;
-                    }
-
-                    @Override
-                    public double mass(Vertex vert) {
-                        return vert.mass;
-                    }
-
-                    @Override
-                    public double force(Vertex vert) {
-                        final double fx = vert.fx;
-                        final double fy = vert.fy;
-                        return Math.sqrt(fx * fx + fy * fy);
-                    }
-
-                    @Override
-                    public void diminishForce(Vertex vert) {
-                        vert.fx /= vert.mass;
-                        vert.fy /= vert.mass;
-                    }
-
-                    @Override
-                    public void addForce(Vertex vert, double dx, double dy) {
-                        vert.fx += dx;
-                        vert.fy += dy;
-                    }
-                };
-            alignTopology(attrs, edges, topModel, Pauser.NULL);
+            Topologies.alignTopology(Vertex.ATTRIBUTION, edges, topModel,
+                                     Pauser.NULL);
             topModel.setData(-1.0, edges);
             System.out.printf("Complete%n");
-        }
-    }
-
-    /**
-     * Allow the vertices of a topology to move around each other to
-     * form a stable, dispersed structure. A simulation is run in which
-     * gravity pushes all vertices apart, and edges behave like ideal
-     * springs. Stop when all vertices are rotating around the centre of
-     * mass uniformly.
-     * 
-     * @param <V> the vertex type
-     * 
-     * @param attrs a means to manipulate the physical attributes of a
-     * vertex
-     * 
-     * @param edges the topology expressed as a set of edges
-     * 
-     * @param display an object to report the status to after each cycle
-     * 
-     * @param pauser an object to regulate reporting
-     */
-    public static <V> void
-        alignTopology(VertexAttribution<V> attrs,
-                      Collection<? extends Edge<? extends Vertex>> edges,
-                      TopologyDisplay<Vertex> display, Pauser pauser) {
-        /* The repulsive gravitational constant */
-        final double push = 0.3;
-
-        /* The maximum allowed speed of any vertex */
-        final double maxSpeed = 0.01;
-
-        /* The strength of air resistance to damp motion */
-        final double airResistance = 0.9;
-
-        /* The maximum simulation step */
-        final double maxDelta = 0.1;
-
-        /* Prepare to detect the end of the simulation. The first two
-         * arrays must have the same length. steadyLimit indicates how
-         * many cycles must pass to stop if the rotation variance is
-         * stable within the threshold given by targetFraction. The
-         * remaining arrays are derived from the first two, and act as
-         * working state or precomputed values during the simulation. */
-        final long[] steadyLimit = { 1000, 10000, 100000 };
-        final double[] targetFraction = { 0.00001, 0.0001, 0.001 };
-        final double[] lowTarget = new double[steadyLimit.length];
-        final double[] highTarget = new double[steadyLimit.length];
-        final long[] steady = new long[steadyLimit.length];
-
-        /* Create an index from each vertex to its neighbours. */
-        final Map<Vertex, Collection<Vertex>> neighbors =
-            Topologies.convertEdgesToNeighbors(edges);
-
-        /* Create a repeatable sequence of vertices, so we can iterate
-         * over unique pairs. */
-        final List<Vertex> arr = new ArrayList<>(neighbors.keySet());
-        final int arrlen = arr.size();
-
-        double elapsed = 0.0;
-        cycles:
-        for (long cycle = 0;; cycle++) {
-            /* Compute forces given current positions and edges. */
-            neighbors.keySet().forEach(Vertex::resetForce);
-
-            /* Apply an elastic force to each edge. */
-            final double optLength = 2.0;
-            final double spring = 0.1;
-            for (Edge<? extends Vertex> entry : edges) {
-                final V a = (V) entry.first();
-                final V b = (V) entry.second();
-                final double dx = attrs.xDiff(b, a);
-                final double dy = attrs.yDiff(b, a);
-                final double r2 = dx * dx + dy * dy;
-                final double dist = Math.sqrt(r2);
-                final double sinth = dy / dist;
-                final double costh = dx / dist;
-                final double force = (dist - optLength) * spring;
-                final double bfx = force * costh;
-                final double bfy = force * sinth;
-                attrs.addForce(a, bfx, bfy);
-                attrs.addForce(b, -bfx, -bfy);
-            }
-
-            /* Make all vertices repel each other. */
-            for (int i = 0; i < arrlen - 1; i++) {
-                for (int j = i + 1; j < arrlen; j++) {
-                    final V a = (V) arr.get(i);
-                    final V b = (V) arr.get(j);
-                    final double dx = attrs.xDiff(b, a);
-                    final double dy = attrs.yDiff(b, a);
-                    final double r2 = dx * dx + dy * dy;
-                    final double r3 = Math.pow(r2, 1.5);
-                    final double bfx = push * dx / r3;
-                    final double bfy = push * dy / r3;
-                    final double bmass = -attrs.mass(b);
-                    final double amass = attrs.mass(a);
-                    attrs.addForce(a, bmass * bfx, bmass * bfy);
-                    attrs.addForce(b, amass * bfx, amass * bfy);
-                }
-            }
-
-            /* Compute air resistance. */
-            neighbors.keySet().forEach(v -> {
-                final double vx = attrs.vx((V) v);
-                final double vy = attrs.vy((V) v);
-                final double sp2 = vx * vx + vy * vy;
-                final double sp = -Math.sqrt(sp2);
-                attrs.addForce((V) v, vx * sp * airResistance,
-                               vy * sp * airResistance);
-            });
-
-            /* Convert the forces to accelerations. */
-            neighbors.keySet().forEach(v -> attrs.diminishForce((V) v));
-
-            if (false) for (Vertex v : neighbors.keySet()) {
-                System.out.printf("  %s: delta-V (%g, %g)%n", v,
-                                  attrs.vx((V) v), attrs.vy((V) v));
-            }
-
-            /* Find the largest time jump we can afford. */
-            double bestDelta = maxDelta;
-            for (Vertex v : neighbors.keySet()) {
-                {
-                    /* Check acceleration. */
-                    final double acc = attrs.force((V) v);
-                    final double delta = maxSpeed / acc;
-                    if (delta < bestDelta) bestDelta = delta;
-                }
-
-                {
-                    /* Check velocity. */
-                    final double sp = attrs.speed((V) v);
-                    final double delta = maxSpeed / sp;
-                    if (delta < bestDelta) bestDelta = delta;
-                }
-            }
-            final double delta =
-                (elapsed + bestDelta > elapsed) ? bestDelta : 0.0001;
-
-            /* Apply all accelerations and velocities. */
-            neighbors.keySet().forEach(v -> attrs.step((V) v, delta));
-            elapsed += delta;
-
-            {
-                /* Recentre by finding the centre of gravity. */
-                double x = 0.0, y = 0.0, mass = 0.0;
-                for (Vertex v : neighbors.keySet()) {
-                    final double vm = attrs.mass((V) v);
-                    mass += vm;
-                    x += vm * attrs.x((V) v);
-                    y += vm * attrs.y((V) v);
-                }
-                final double xshift = x / mass;
-                final double yshift = y / mass;
-                neighbors.keySet()
-                    .forEach(v -> attrs.move((V) v, -xshift, -yshift));
-            }
-
-            {
-                /* Compute mean rotation. */
-                double sum = 0.0, sqsum = 0.0;
-                for (Vertex v : neighbors.keySet()) {
-                    final double x = attrs.x((V) v);
-                    final double y = attrs.y((V) v);
-                    final double r2 = x * x + y * y;
-                    final double rot =
-                        (attrs.vy((V) v) * x - attrs.vx((V) v) * y) / r2;
-                    sum += rot;
-                    sqsum += rot * rot;
-                }
-                sum /= neighbors.size();
-                sqsum /= neighbors.size();
-                final double var = sqsum - sum * sum;
-                // final double stddev = Math.sqrt(var);
-                if (false)
-                    System.out.printf("rotation: (var %g) %d %d %d = %d%n",
-                                      var, steady[0], steady[1], steady[2],
-                                      cycle);
-
-                /* Detect stability in the variance. */
-                final double signal = var;
-                for (int i = 0; i < steady.length; i++) {
-                    if (signal > highTarget[i] || signal < lowTarget[i]) {
-                        lowTarget[i] = signal * (1.0 - targetFraction[i]);
-                        highTarget[i] = signal * (1.0 + targetFraction[i]);
-                        steady[i] = cycle;
-                    }
-                }
-            }
-
-            if (false) {
-                System.out.printf("%nElapsed: %gs (by %gs)%n", elapsed,
-                                  delta);
-                for (Vertex v : neighbors.keySet()) {
-                    System.out.printf("  %s: (%g, %g)%n", v, attrs.x((V) v),
-                                      attrs.y((V) v));
-                }
-            }
-            pauser.pause(cycle);
-
-            {
-                /* Report the latest positions. */
-                final double minDelta = 1e-6;
-                assert delta <= maxDelta;
-                final double ratio =
-                    (Math.log(Math.max(minDelta, delta)) - Math.log(minDelta))
-                        / (Math.log(maxDelta) - Math.log(minDelta));
-                assert ratio >= 0.0;
-                assert ratio <= 1.0;
-                display.setData(ratio, edges);
-            }
-
-            if (false) {
-                /* Find the average velocity. */
-                double vxsum = 0.0, vysum = 0.0;
-                for (Vertex v : neighbors.keySet()) {
-                    vxsum += attrs.vx((V) v);
-                    vysum += attrs.vy((V) v);
-                }
-                final double vxmean = vxsum / neighbors.size();
-                final double vymean = vysum / neighbors.size();
-
-                /* What's the total energy in the system? */
-                double energy = 0.0;
-                double mass = 0.0;
-                for (Vertex v : neighbors.keySet()) {
-                    final double vx = attrs.x((V) v) - vxmean;
-                    final double vy = attrs.y((V) v) - vymean;
-                    final double vm = attrs.mass((V) v);
-                    energy += vm * (vx * vx + vy * vy) / 2.0;
-                    mass += vm;
-                }
-                System.out.printf("Energy density: %g (delta %g)%n",
-                                  energy / mass, delta);
-            }
-
-            // if (cycle > 100 && delta == maxDelta) break;
-            for (int i = 0; i < steady.length; i++)
-                if (cycle - steady[i] > steadyLimit[i]) break cycles;
         }
     }
 
--- src/java-tree/tests/Topologies.java
+++ src/java-tree/tests/Topologies.java
@@ -35,9 +35,11 @@
  * Author: Steven Simpson <s.simpson@lancaster.ac.uk>
  */
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.IntSupplier;
@@ -158,6 +160,245 @@ public final class Topologies {
                     edgeCount += 2;
                 }
             }
+        }
+    }
+
+    /**
+     * Allow the vertices of a topology to move around each other to
+     * form a stable, dispersed structure. A simulation is run in which
+     * gravity pushes all vertices apart, and edges behave like ideal
+     * springs. Stop when all vertices are rotating around the centre of
+     * mass uniformly.
+     * 
+     * @param <V> the vertex type
+     * 
+     * @param attrs a means to manipulate the physical attributes of a
+     * vertex
+     * 
+     * @param edges the topology expressed as a set of edges
+     * 
+     * @param display an object to report the status to after each cycle
+     * 
+     * @param pauser an object to regulate reporting
+     */
+    public static <V> void
+        alignTopology(VertexAttribution<V> attrs,
+                      Collection<? extends Edge<? extends V>> edges,
+                      TopologyDisplay<V> display, Pauser pauser) {
+        /* The repulsive gravitational constant */
+        final double push = 0.3;
+    
+        /* The maximum allowed speed of any vertex */
+        final double maxSpeed = 0.01;
+    
+        /* The strength of air resistance to damp motion */
+        final double airResistance = 0.9;
+    
+        /* The maximum simulation step */
+        final double maxDelta = 0.1;
+    
+        /* Prepare to detect the end of the simulation. The first two
+         * arrays must have the same length. steadyLimit indicates how
+         * many cycles must pass to stop if the rotation variance is
+         * stable within the threshold given by targetFraction. The
+         * remaining arrays are derived from the first two, and act as
+         * working state or precomputed values during the simulation. */
+        final long[] steadyLimit = { 1000, 10000, 100000 };
+        final double[] targetFraction = { 0.00001, 0.0001, 0.001 };
+        final double[] lowTarget = new double[steadyLimit.length];
+        final double[] highTarget = new double[steadyLimit.length];
+        final long[] steady = new long[steadyLimit.length];
+    
+        /* Create an index from each vertex to its neighbours. */
+        final Map<V, Collection<V>> neighbors =
+            convertEdgesToNeighbors(edges);
+    
+        /* Create a repeatable sequence of vertices, so we can iterate
+         * over unique pairs. */
+        final List<V> arr = new ArrayList<>(neighbors.keySet());
+        final int arrlen = arr.size();
+    
+        double elapsed = 0.0;
+        cycles:
+        for (long cycle = 0;; cycle++) {
+            /* Compute forces given current positions and edges. */
+            neighbors.keySet().forEach(v -> attrs.resetForce(v));
+    
+            /* Apply an elastic force to each edge. */
+            final double optLength = 2.0;
+            final double spring = 0.1;
+            for (Edge<? extends V> entry : edges) {
+                final V a = entry.first();
+                final V b = entry.second();
+                final double dx = attrs.xDiff(b, a);
+                final double dy = attrs.yDiff(b, a);
+                final double r2 = dx * dx + dy * dy;
+                final double dist = Math.sqrt(r2);
+                final double sinth = dy / dist;
+                final double costh = dx / dist;
+                final double force = (dist - optLength) * spring;
+                final double bfx = force * costh;
+                final double bfy = force * sinth;
+                attrs.addForce(a, bfx, bfy);
+                attrs.addForce(b, -bfx, -bfy);
+            }
+    
+            /* Make all vertices repel each other. */
+            for (int i = 0; i < arrlen - 1; i++) {
+                for (int j = i + 1; j < arrlen; j++) {
+                    final V a = arr.get(i);
+                    final V b = arr.get(j);
+                    final double dx = attrs.xDiff(b, a);
+                    final double dy = attrs.yDiff(b, a);
+                    final double r2 = dx * dx + dy * dy;
+                    final double r3 = Math.pow(r2, 1.5);
+                    final double bfx = push * dx / r3;
+                    final double bfy = push * dy / r3;
+                    final double bmass = -attrs.mass(b);
+                    final double amass = attrs.mass(a);
+                    attrs.addForce(a, bmass * bfx, bmass * bfy);
+                    attrs.addForce(b, amass * bfx, amass * bfy);
+                }
+            }
+    
+            /* Compute air resistance. */
+            neighbors.keySet().forEach(v -> {
+                final double vx = attrs.vx(v);
+                final double vy = attrs.vy(v);
+                final double sp2 = vx * vx + vy * vy;
+                final double sp = -Math.sqrt(sp2);
+                attrs.addForce(v, vx * sp * airResistance,
+                               vy * sp * airResistance);
+            });
+    
+            /* Convert the forces to accelerations. */
+            neighbors.keySet().forEach(v -> attrs.diminishForce(v));
+    
+            if (false) for (V v : neighbors.keySet()) {
+                System.out.printf("  %s: delta-V (%g, %g)%n", v, attrs.vx(v),
+                                  attrs.vy(v));
+            }
+    
+            /* Find the largest time jump we can afford. */
+            double bestDelta = maxDelta;
+            for (V v : neighbors.keySet()) {
+                {
+                    /* Check acceleration. */
+                    final double acc = attrs.force(v);
+                    final double delta = maxSpeed / acc;
+                    if (delta < bestDelta) bestDelta = delta;
+                }
+    
+                {
+                    /* Check velocity. */
+                    final double sp = attrs.speed(v);
+                    final double delta = maxSpeed / sp;
+                    if (delta < bestDelta) bestDelta = delta;
+                }
+            }
+            final double delta =
+                (elapsed + bestDelta > elapsed) ? bestDelta : 0.0001;
+    
+            /* Apply all accelerations and velocities. */
+            neighbors.keySet().forEach(v -> attrs.step(v, delta));
+            elapsed += delta;
+    
+            {
+                /* Recentre by finding the centre of gravity. */
+                double x = 0.0, y = 0.0, mass = 0.0;
+                for (V v : neighbors.keySet()) {
+                    final double vm = attrs.mass(v);
+                    mass += vm;
+                    x += vm * attrs.x(v);
+                    y += vm * attrs.y(v);
+                }
+                final double xshift = x / mass;
+                final double yshift = y / mass;
+                neighbors.keySet()
+                    .forEach(v -> attrs.move(v, -xshift, -yshift));
+            }
+    
+            {
+                /* Compute mean rotation. */
+                double sum = 0.0, sqsum = 0.0;
+                for (V v : neighbors.keySet()) {
+                    final double x = attrs.x(v);
+                    final double y = attrs.y(v);
+                    final double r2 = x * x + y * y;
+                    final double rot =
+                        (attrs.vy(v) * x - attrs.vx(v) * y) / r2;
+                    sum += rot;
+                    sqsum += rot * rot;
+                }
+                sum /= neighbors.size();
+                sqsum /= neighbors.size();
+                final double var = sqsum - sum * sum;
+                // final double stddev = Math.sqrt(var);
+                if (false)
+                    System.out.printf("rotation: (var %g) %d %d %d = %d%n",
+                                      var, steady[0], steady[1], steady[2],
+                                      cycle);
+    
+                /* Detect stability in the variance. */
+                final double signal = var;
+                for (int i = 0; i < steady.length; i++) {
+                    if (signal > highTarget[i] || signal < lowTarget[i]) {
+                        lowTarget[i] = signal * (1.0 - targetFraction[i]);
+                        highTarget[i] = signal * (1.0 + targetFraction[i]);
+                        steady[i] = cycle;
+                    }
+                }
+            }
+    
+            if (false) {
+                System.out.printf("%nElapsed: %gs (by %gs)%n", elapsed,
+                                  delta);
+                for (V v : neighbors.keySet()) {
+                    System.out.printf("  %s: (%g, %g)%n", v, attrs.x(v),
+                                      attrs.y(v));
+                }
+            }
+            pauser.pause(cycle);
+    
+            {
+                /* Report the latest positions. */
+                final double minDelta = 1e-6;
+                assert delta <= maxDelta;
+                final double ratio =
+                    (Math.log(Math.max(minDelta, delta)) - Math.log(minDelta))
+                        / (Math.log(maxDelta) - Math.log(minDelta));
+                assert ratio >= 0.0;
+                assert ratio <= 1.0;
+                display.setData(ratio, edges);
+            }
+    
+            if (false) {
+                /* Find the average velocity. */
+                double vxsum = 0.0, vysum = 0.0;
+                for (V v : neighbors.keySet()) {
+                    vxsum += attrs.vx(v);
+                    vysum += attrs.vy(v);
+                }
+                final double vxmean = vxsum / neighbors.size();
+                final double vymean = vysum / neighbors.size();
+    
+                /* What's the total energy in the system? */
+                double energy = 0.0;
+                double mass = 0.0;
+                for (V v : neighbors.keySet()) {
+                    final double vx = attrs.x(v) - vxmean;
+                    final double vy = attrs.y(v) - vymean;
+                    final double vm = attrs.mass(v);
+                    energy += vm * (vx * vx + vy * vy) / 2.0;
+                    mass += vm;
+                }
+                System.out.printf("Energy density: %g (delta %g)%n",
+                                  energy / mass, delta);
+            }
+    
+            // if (cycle > 100 && delta == maxDelta) break;
+            for (int i = 0; i < steady.length; i++)
+                if (cycle - steady[i] > steadyLimit[i]) break cycles;
         }
     }
 
