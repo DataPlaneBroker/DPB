36ad6541dd5672ea931b3be884185e528c5cef18
simpsons <simpsons@eb0d62cf-e8a8-483d-89ee-368caa62dd19>
2019-06-27 09:26:22 +0000

    Topology converted to dummy broker network.

--- src/java-tree/tests/AlgoPerfTest.java
+++ src/java-tree/tests/AlgoPerfTest.java
@@ -38,9 +38,24 @@
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
 
+import uk.ac.lancs.networks.NetworkControl;
+import uk.ac.lancs.networks.Terminal;
+import uk.ac.lancs.networks.mgmt.Aggregator;
+import uk.ac.lancs.networks.mgmt.SubterminalBusyException;
+import uk.ac.lancs.networks.mgmt.Switch;
+import uk.ac.lancs.networks.mgmt.TerminalId;
+import uk.ac.lancs.networks.mgmt.TerminalNameException;
+import uk.ac.lancs.networks.mgmt.Trunk;
+import uk.ac.lancs.networks.mgmt.UnknownSubnetworkException;
+import uk.ac.lancs.networks.mgmt.UnknownSubterminalException;
+import uk.ac.lancs.networks.transients.DummySwitch;
+import uk.ac.lancs.networks.transients.TransientAggregator;
 import uk.ac.lancs.routing.span.Edge;
 
 /**
@@ -53,7 +68,7 @@ public class AlgoPerfTest {
      * @param args
      */
     public static void main(String[] args) throws Exception {
-        /* Create a scale-free network. */
+        /* Create a scale-free topology. */
         final int vertexCount = 100;
         final int newEdgesPerVertex = 3;
         Collection<Edge<Vertex>> edges = new HashSet<>();
@@ -78,5 +93,65 @@ public class AlgoPerfTest {
                                      (sp, ed) -> {}, Pauser.NULL);
             System.out.printf("Complete%n");
         }
+
+        Map<Vertex, Collection<Vertex>> neighbors =
+            Topologies.convertEdgesToNeighbors(edges);
+
+        /* Keep track of vertices that have already had switches created
+         * for them. */
+        Map<Vertex, Switch> vertexToSwitch = new IdentityHashMap<>();
+
+        Map<String, NetworkControl> subnets = new HashMap<>();
+        Aggregator aggr =
+            new TransientAggregator(Executors.newCachedThreadPool(), "aggr",
+                                    subnets::get);
+        /* Keep a set of aggregator terminals to use for test circuits,
+         * and the proportion of likelihood of being chosen. */
+        Map<Terminal, Double> candidates = new HashMap<>();
+
+        /* Build a transient broker network out of the topology. */
+        for (Edge<Vertex> edge : edges) {
+            /* Ensure the switches corresponding to these vertices
+             * exist. Derive their names from the vertices, and ensure
+             * they have one initial terminal that will not be used to
+             * form trunks. */
+            Vertex v1 = edge.first();
+            int d1 = neighbors.get(v1).size();
+            Vertex v2 = edge.second();
+            int d2 = neighbors.get(v2).size();
+            Function<Vertex, DummySwitch> maker = v -> {
+                String name = "v" + v;
+                DummySwitch r = new DummySwitch(name);
+                subnets.put(name, r.getControl());
+                try {
+                    Terminal t = r.addTerminal("host", "meaningless");
+                    Terminal st = aggr.addTerminal("a" + v, TerminalId
+                        .of(r.getControl().name(), t.name()));
+                    int d = neighbors.get(v).size();
+                    candidates.put(st, 1.0 / d);
+                } catch (SubterminalBusyException
+                    | UnknownSubterminalException | TerminalNameException
+                    | UnknownSubnetworkException e) {
+                    throw new AssertionError("unreachable", e);
+                }
+                return r;
+            };
+            Switch s1 = vertexToSwitch.computeIfAbsent(v1, maker);
+            Switch s2 = vertexToSwitch.computeIfAbsent(v2, maker);
+
+            /* Create terminals to connect as a trunk. */
+            Terminal t1 = s1.addTerminal("to" + v2, v2.toString());
+            Terminal t2 = s2.addTerminal("to" + v1, v1.toString());
+
+            /* Create a trunk between the switches. Give it a few
+             * hundred labels, and bandwidth proportional to the smaller
+             * degree of its two vertices. */
+            Trunk trunk = aggr
+                .addTrunk(TerminalId.of(s1.getControl().name(), t1.name()),
+                          TerminalId.of(s2.getControl().name(), t2.name()));
+            trunk.defineLabelRange(1000, 300);
+            trunk.provideBandwidth(1000.0 * Math.min(d1, d2));
+        }
+
     }
 }
