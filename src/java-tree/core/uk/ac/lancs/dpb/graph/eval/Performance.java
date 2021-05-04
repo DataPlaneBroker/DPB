3f9cd46b8a6aed8f4cc0be13768d1ac3c9e16a41
Steven Simpson <s.simpson@lancaster.ac.uk>
2021-05-04 11:14:26 +0100

    Tidied up experiment.

--- src/java-tree/core/uk/ac/lancs/dpb/graph/eval/Performance.java
+++ src/java-tree/core/uk/ac/lancs/dpb/graph/eval/Performance.java
@@ -38,7 +38,6 @@ package uk.ac.lancs.dpb.graph.eval;
 
 import java.io.File;
 import java.io.PrintWriter;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -60,15 +59,44 @@ import uk.ac.lancs.dpb.tree.TreePlotter;
  */
 public class Performance {
     public static void main(String[] args) throws Exception {
+        /* This is the number of vertices in each generated graph. */
+        final int[] vertexCounts =
+            IntStream.rangeClosed(1, 1).map(i -> i * 50).toArray();
+
+        /* This is the number of times we use one graph with all
+         * variations of other parameters. */
+        final int graphRuns = 10;
+
+        /* This is the number of goals we select to create a challenge
+         * from a graph. */
+        final int[] goalSetSizes = IntStream.rangeClosed(3, 5).toArray();
+
+        /* This is the number of times we use goal sets of the same
+         * size. */
+        final int goalSetRuns = 10;
+
+        class Measurement {
+            final double elapsed;
+
+            final double fit;
+
+            public Measurement(double elapsed, double fit) {
+                this.elapsed = elapsed;
+                this.fit = fit;
+            }
+
+            @Override
+            public String toString() {
+                return String.format("%g,%g", elapsed, fit);
+            }
+        }
+
         /* algo, grid size, grid number, goal count, challenge number */
         Map<String,
             Map<Integer,
-                Map<Integer, Map<Integer, Map<Integer, Double>>>>> elapsed =
-                    new HashMap<>();
-        Map<String,
-            Map<Integer,
-                Map<Integer, Map<Integer, Map<Integer, Double>>>>> fit =
-                    new HashMap<>();
+                Map<Integer,
+                    Map<Integer, Map<Integer, Measurement>>>>> measurements =
+                        new LinkedHashMap<>();
 
         Random rng = new Random(1);
 
@@ -94,9 +122,10 @@ public class Performance {
 
         /* Vary the graph dimensions. Do this in the outermost loop
          * because some of these are quite costly. */
-        for (int vertexCount = 50; vertexCount <= 50; vertexCount += 50) {
+        for (final int vertexCount : vertexCounts) {
             /* Generate several graphs of this size. */
-            for (int graphIter = 0; graphIter < 10; graphIter++) {
+            for (final int graphIter : IntStream.range(0, graphRuns)
+                .toArray()) {
                 System.out.printf("%nVertices: %d; run %d%n", vertexCount,
                                   graphIter);
                 Graph graph = GraphExamples
@@ -110,15 +139,14 @@ public class Performance {
                 try (PrintWriter out = new PrintWriter(new File(String
                     .format("scratch/graph-%d-%d.svg", vertexCount,
                             graphIter)))) {
-                    graph.drawSVG(out, null, null, 0.2, 0.3);
+                    graph.drawSVG(out, null, null, 0.3, 0.4);
                 }
 
                 /* Vary the number of goals. */
-                for (int goalCountIter = 3; goalCountIter <= 5;
-                     goalCountIter++) {
-                    final int goalCount = goalCountIter;
+                for (final int goalCount : goalSetSizes) {
                     /* Run several challenges on this graph. */
-                    for (int chalIter = 0; chalIter < 10; chalIter++) {
+                    for (final int chalIter : IntStream.range(0, goalSetRuns)
+                        .toArray()) {
                         List<Vertex> goals = graph.chooseGoals(goalCount, rng);
                         assert goals.size() == goalCount;
 
@@ -126,124 +154,130 @@ public class Performance {
                             .format("scratch/challenge-%d-%d-%d-%d.svg",
                                     vertexCount, graphIter, goalCount,
                                     chalIter)))) {
-                            graph.drawSVG(out, goals, null, 0.2, 0.3);
+                            graph.drawSVG(out, goals, null, 0.3, 0.4);
                         }
 
-                        if (true) {
-                            /* Allow each goal to send the same amount,
-                             * and receive the sum of the others. */
-                            DemandFunction demand =
-                                new PairDemandFunction(IntStream
-                                    .range(0, goals.size())
-                                    .mapToObj(i -> BidiCapacity
-                                        .of(Capacity.at(1.0),
-                                            Capacity.at(1.0 / (goalCount - 1))))
-                                    .collect(Collectors.toList()));
+                        /* Allow each goal to send the same amount, and
+                         * receive the sum of the others. */
+                        DemandFunction demand = new PairDemandFunction(IntStream
+                            .range(0, goals.size())
+                            .mapToObj(i -> BidiCapacity
+                                .of(Capacity.at(1.0),
+                                    Capacity.at(1.0 / (goalCount - 1))))
+                            .collect(Collectors.toList()));
 
-                            /* Measure the performance and accuracy of
-                             * each algorithm. */
-                            for (var namedAlgo : algos.entrySet()) {
-                                String name = namedAlgo.getKey();
-                                TreePlotter algo = namedAlgo.getValue();
+                        /* Measure the performance and accuracy of each
+                         * algorithm. */
+                        for (var namedAlgo : algos.entrySet()) {
+                            String name = namedAlgo.getKey();
+                            TreePlotter algo = namedAlgo.getValue();
 
-                                System.err.printf("Algo: %s%n", name);
-                                final double duration;
-                                double bestScore = Double.POSITIVE_INFINITY;
-                                Map<? extends QualifiedEdge<Vertex>,
-                                    ? extends BidiCapacity> best = null;
-                                final long start = System.currentTimeMillis();
-                                for (int cycles = 1;; cycles++) {
-                                    for (var cand : algo.plot(goals, demand,
-                                                              graph.edges)) {
-                                        double score = 0.0;
-                                        for (var entry : cand.entrySet()) {
-                                            QualifiedEdge<Vertex> key =
-                                                entry.getKey();
-                                            BidiCapacity val = entry.getValue();
-                                            score +=
-                                                key.cost * (val.ingress.min()
-                                                    + val.egress.min());
-                                        }
-                                        if (best == null || score < bestScore) {
-                                            best = cand;
-                                            bestScore = score;
-                                            if (false) System.err
-                                                .printf("acc %g: %s%n",
-                                                        bestScore,
-                                                        best.entrySet().stream()
-                                                            .map(e -> e.getKey()
-                                                                .toString())
-                                                            .collect(Collectors
-                                                                .joining(", ")));
-                                        }
+                            // System.err.printf("Algo: %s%n", name);
+                            final double duration;
+                            double bestScore = Double.POSITIVE_INFINITY;
+                            Map<? extends QualifiedEdge<Vertex>,
+                                ? extends BidiCapacity> best = null;
+                            final long start = System.currentTimeMillis();
+                            for (int cycles = 1;; cycles++) {
+                                for (var cand : algo.plot(goals, demand,
+                                                          graph.edges)) {
+                                    double score = 0.0;
+                                    for (var entry : cand.entrySet()) {
+                                        QualifiedEdge<Vertex> key =
+                                            entry.getKey();
+                                        BidiCapacity val = entry.getValue();
+                                        score += key.cost * (val.ingress.min()
+                                            + val.egress.min());
                                     }
-
-                                    /* Compute the duration. If it's not
-                                     * yet added up to a second, tryin
-                                     * again. */
-                                    final long stop =
-                                        System.currentTimeMillis();
-                                    if (stop - start < 2000) continue;
-
-                                    /* Record the average duration, and
-                                     * stop. */
-                                    duration = (stop - start) / 1000.0 / cycles;
-                                    break;
+                                    if (best == null || score < bestScore) {
+                                        best = cand;
+                                        bestScore = score;
+                                        if (false) System.err
+                                            .printf("acc %g: %s%n", bestScore,
+                                                    best.entrySet().stream()
+                                                        .map(e -> e.getKey()
+                                                            .toString())
+                                                        .collect(Collectors
+                                                            .joining(", ")));
+                                    }
                                 }
 
-                                /* algo, vertexCount, graphIter,
-                                 * goalCount, chalter */
-                                elapsed
-                                    .computeIfAbsent(name, k -> new TreeMap<>())
-                                    .computeIfAbsent(vertexCount,
-                                                     k -> new TreeMap<>())
-                                    .computeIfAbsent(graphIter,
-                                                     k -> new TreeMap<>())
-                                    .computeIfAbsent(goalCount,
-                                                     k -> new TreeMap<>())
-                                    .put(chalIter, duration);
+                                /* Compute the duration. If it's not yet
+                                 * added up to a second, tryin again. */
+                                final long stop = System.currentTimeMillis();
+                                if (stop - start < 2000) continue;
 
-                                File svg = new File(String
-                                    .format("scratch/solution-%d-%d-%d-%d-%s.svg",
-                                            vertexCount, graphIter, goalCount,
-                                            chalIter, name));
-                                if (best != null) {
-                                    fit.computeIfAbsent(name,
-                                                        k -> new TreeMap<>())
-                                        .computeIfAbsent(vertexCount,
-                                                         k -> new TreeMap<>())
-                                        .computeIfAbsent(graphIter,
-                                                         k -> new TreeMap<>())
-                                        .computeIfAbsent(goalCount,
-                                                         k -> new TreeMap<>())
-                                        .put(chalIter, bestScore);
-                                    try (PrintWriter out =
-                                        new PrintWriter(svg)) {
-                                        graph.drawSVG(out, goals, best, 0.2,
-                                                      0.3);
-                                    }
-                                    System.err.printf("%d vertices; graph %d;"
-                                        + " %d goals;" + " goalset %d;" + " %s"
-                                        + " took %g s to find" + " best %g%n",
-                                                      vertexCount, graphIter,
-                                                      goalCount, chalIter, name,
-                                                      duration, bestScore);
-                                } else {
-                                    svg.delete();
-                                    System.err.printf("%d vertices; graph %d;"
-                                        + " %d goals;" + " goalset %d;" + " %s"
-                                        + " took %g s to find"
-                                        + " no solution%n", vertexCount,
-                                                      graphIter, goalCount,
-                                                      chalIter, name, duration);
-                                }
+                                /* Record the average duration, and
+                                 * stop. */
+                                duration = (stop - start) / 1000.0 / cycles;
+                                break;
                             }
+
+                            File svg = new File(String
+                                .format("scratch/solution-%d-%d-%d-%d-%s.svg",
+                                        vertexCount, graphIter, goalCount,
+                                        chalIter, name));
+                            final Measurement measurement;
+                            if (best != null) {
+                                measurement =
+                                    new Measurement(duration, bestScore);
+                                try (PrintWriter out = new PrintWriter(svg)) {
+                                    graph.drawSVG(out, goals, best, 0.3, 0.4);
+                                }
+                                System.err.printf("%d vertices; graph %d;"
+                                    + " %d goals;" + " goalset %d;" + " %s"
+                                    + " took %g s to find" + " best %g%n",
+                                                  vertexCount, graphIter,
+                                                  goalCount, chalIter, name,
+                                                  duration, bestScore);
+                            } else {
+                                measurement =
+                                    new Measurement(duration,
+                                                    Double.POSITIVE_INFINITY);
+                                svg.delete();
+                                System.err.printf("%d vertices; graph %d;"
+                                    + " %d goals;" + " goalset %d;" + " %s"
+                                    + " took %g s to find" + " no solution%n",
+                                                  vertexCount, graphIter,
+                                                  goalCount, chalIter, name,
+                                                  duration);
+                            }
+
+                            /* algo, vertexCount, graphIter, goalCount,
+                             * chalter */
+                            measurements
+                                .computeIfAbsent(name, k -> new TreeMap<>())
+                                .computeIfAbsent(vertexCount,
+                                                 k -> new TreeMap<>())
+                                .computeIfAbsent(graphIter,
+                                                 k -> new TreeMap<>())
+                                .computeIfAbsent(goalCount,
+                                                 k -> new TreeMap<>())
+                                .put(chalIter, measurement);
                         }
                     }
                 }
             }
         }
-        System.out.printf("Elapsed:%n%s%n", elapsed);
-        System.out.printf("Scores:%n%s%n", fit);
+        System.out.printf("Measurements:%n%s%n", measurements);
+        for (var e1 : measurements.entrySet()) {
+            final String algo = e1.getKey();
+            for (var e2 : e1.getValue().entrySet()) {
+                final int gridSize = e2.getKey();
+                for (var e3 : e2.getValue().entrySet()) {
+                    final int gridInstance = e3.getKey();
+                    for (var e4 : e3.getValue().entrySet()) {
+                        final int goalCount = e4.getKey();
+                        for (var e5 : e4.getValue().entrySet()) {
+                            final int goalSet = e5.getKey();
+                            final Measurement m = e5.getValue();
+                            System.out.printf("%s,%d,%d,%d,%d,%s%n", algo,
+                                              gridSize, gridInstance, goalCount,
+                                              goalSet, m);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
